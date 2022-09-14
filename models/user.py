f59576c011be16b70fcded91d7523ea98a684127
Rawi Daniel <rawidaniel96@gmail.com>
2022-09-14 00:00:29 -0700

    update place class

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String
-
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
@@ -13,6 +13,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship('Place', backref='user',
+                              cascade="all, delete, delete-orphan")
     else:
         email = ''
         password = ''
