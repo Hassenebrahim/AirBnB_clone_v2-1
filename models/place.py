e6b37c8ab07e71211351dd8cf4db1212fc86ce65
Rawi Daniel <rawidaniel96@gmail.com>
2022-09-14 00:17:04 -0700

    fix output erro

--- models/place.py
+++ models/place.py
@@ -14,6 +14,7 @@ class Place(BaseModel, Base):
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
         max_guest = Column(Integer, nullable=False, default=0)
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
